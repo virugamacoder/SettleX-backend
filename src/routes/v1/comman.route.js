76f8f9ff10c58a60c706cc2ff0cd200ff7c3fdb6
virugamacoder <virugama0@gmail.com>
2026-03-09 19:09:17 +0530

    feat: implement admin role management script with add, remove, and list functionalities

--- src/routes/v1/comman.route.js
+++ src/routes/v1/comman.route.js
@@ -10,8 +10,13 @@ const catchAsync = require('../../utils/catchAsync');
 const router = express.Router();
 
 router.post('/upload/', uploadStorage.single('file'), catchAsync((req, res) => {
-  const url = req.file.path;
-  sendSuccessResponse(res, 'file uploaded successfully', url, httpStatus.CREATED);
+  if (!req.file) {
+    return res.status(400).json({ status: 'fail', message: 'No file uploaded' });
+  }
+  const protocol = req.headers['x-forwarded-proto'] || req.protocol;
+  const host = req.headers['x-forwarded-host'] || req.get('host');
+  const fileUrl = `${protocol}://${host}/${req.file.path}`;
+  sendSuccessResponse(res, 'file uploaded successfully', fileUrl, httpStatus.CREATED);
 }));
 
 module.exports = router;
